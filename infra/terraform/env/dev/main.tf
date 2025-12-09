85e9c6c12c31114fb55f8a45285ab7df09c2b33b
shoebimtiaz <shoebimtiaz@gmail.com>
2025-12-09 17:13:40 -0600

    added networking

--- infra/terraform/env/dev/main.tf
+++ infra/terraform/env/dev/main.tf
@@ -23,3 +23,24 @@ resource "azurerm_user_assigned_identity" "dev_identity" {
     location = azurerm_resource_group.dev_rg.location
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "bookapp-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
