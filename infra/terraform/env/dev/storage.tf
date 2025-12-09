261c42fc1111d446b0d19a3bd378f390007a809e
shoebimtiaz <shoebimtiaz@gmail.com>
2025-12-09 15:21:15 -0600

    added storage container

--- infra/terraform/env/dev/storage.tf
+++ infra/terraform/env/dev/storage.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_acccount" "logs_storage_account" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = "storageaccount"
   resource_group_name      = azurerm_resource_group.dev_rg.name
   location                 = azurerm_resource_group.dev_rg.location
@@ -6,11 +6,14 @@ resource "azurerm_storage_acccount" "logs_storage_account" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_container" "logs_container" {
+    name = "logs"
+    storage_account_id = azurerm_storage_account.storage_account.id
+    container_access_type = "private"
+}
 
-resource "azurerm_storage_account" "images_storage_account" {
-  name                     = "imagestorageacct"
-  resource_group_name      = azurerm_resource_group.dev_rg.name
-  location                 = azurerm_resource_group.dev_rg.location
-  account_tier             = "Free Tier"
-  account_replication_type = "LRS"
+resource "azurerm_storage_container" "images_container" {
+    name = "images"
+    storage_account_id = azurerm_storage_account.storage_account.id
+    container_access_type = "private"
 }
